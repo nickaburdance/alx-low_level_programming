52bbd5213252acfb66bd26b35d3a02a1c9a773d8
nickaburdance <nickaburdance.com@gmail.com>
2022-12-20 03:41:04 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; str[i] != '\n'; i++)
+	for (; str[i] != '\0'; i++)
 	{
 		if ((i % 2) == 0)
 		{
 			if ((i % 2) == 0)
 				_putchar(str[i]);
 			else
-				continue
+				continue;
 	}
 	_putchar('\n');
 }
